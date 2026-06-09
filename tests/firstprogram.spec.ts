84451557d2253ded6cb01219a5a8dd7d97de2d05
Vysh <vyshnavi.iyappan@redembedded.com>
2026-06-09 21:25:36 +0100

    Initial commit

---
+++ tests/firstprogram.spec.ts
@@ -0,0 +1,16 @@
+// We are importing test and expect from playwright package
+import {test, expect} from '@playwright/test';
+
+test("First test case", () => {
+
+})
+
+
+test("Second test case", () => {
+
+})
+
+
+test("Third test case", () => {
+
+})
