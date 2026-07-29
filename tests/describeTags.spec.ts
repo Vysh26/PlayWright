2fa45088bdf35c3188e5f7dd8fe8b836638b6973
Vysh <vyshnavi.iyappan@redembedded.com>
2026-07-29 21:13:09 +0100

    Added describe tests

---
+++ tests/describeTags.spec.ts
@@ -0,0 +1,18 @@
+import {test, expect} from '@playwright/test';
+
+test.describe('login Group test cases', () => {
+
+test ('test script 1', ({page}) => {
+    console.log('login test script 1')
+})
+
+test ('test script 2', ({page}) => {
+    console.log('login test script 2')
+})
+
+test ('test script 3', ({page}) => {
+    console.log('login test script 3')
+})
+})
+
+
