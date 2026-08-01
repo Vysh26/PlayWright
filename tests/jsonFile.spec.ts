e2c4941269ef4b8f947a52d8d25913719e4c6c21
Vysh <vyshnavi.iyappan@redembedded.com>
2026-08-01 10:50:00 +0100

    Added Read file json

---
+++ tests/jsonFile.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+import fs from 'fs';
+
+test('Read JSON using fs', async () => {
+
+    const data = fs.readFileSync('testdata/testData.json', 'utf-8');
+    const json = JSON.parse(data);
+
+    console.log(json.username);
+});
