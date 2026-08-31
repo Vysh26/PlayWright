6e7ff03fd13669e91572acfa603e8e102b2b4d5b
unknown <Vysh>
2026-08-31 14:39:05 +0100

    Schema validation

---
+++ tests/schema.spec.ts
@@ -0,0 +1,7 @@
+
+import {test, expect} from '@playwright/test';
+import {validateSchemaZod} from 'playwright-schema-validator';
+import {z} from 'zod';
+
+describe('Schema Validation Tests', () => {
+})
